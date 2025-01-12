c936876606b9eb84c2d93272536f8ba81412db46
rubayat-rafi <rubayatrafi522@gmail.com>
2025-01-12 14:37:04 +0600

    add plant section create'

---
+++ client/src/components/Home/Card.jsx
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const Card = ({plant}) => {
+
+  console.log(plant)
+
+  const {name, category, quantity, price, imageURL, _id} = plant || {}
+
+  return (
+    <Link
+      to={`/plant/${_id}`}
+      className='col-span-1 cursor-pointer group shadow-xl p-3 rounded-xl'
+    >
+      <div className='flex flex-col gap-2 w-full'>
+        <div
+          className='
+              aspect-square 
+              w-full 
+              relative 
+              overflow-hidden 
+              rounded-xl
+            '
+        >
+          <img
+            className='
+                object-cover 
+                h-full 
+                w-full 
+                group-hover:scale-110 
+                transition
+              '
+            src={imageURL}
+            alt='Plant Image'
+          />
+          <div
+            className='
+              absolute
+              top-3
+              right-3
+            '
+          ></div>
+        </div>
+        <div className='font-semibold text-lg'>{name}</div>
+        <div className='font-semibold text-lg'>Category: {category}</div>
+        <div className='font-semibold text-lg'>Quantity: {quantity}</div>
+        <div className='flex flex-row items-center gap-1'>
+          <div className='font-semibold'> Price: {price}$</div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+
+Card.propTypes = {
+  plant: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+export default Card
+
---
+++ client/src/pages/Dashboard/Seller/AddPlant.jsx
@@ -0,0 +1,85 @@
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { imageUpload } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { axiosSecure } from "../../../hooks/useAxiosSecure";
+
+
+
+const AddPlant = () => {
+  const { user } = useAuth();
+  const [uploadImage, setUploadImage] = useState({
+    image: {name: "Upload Image"},
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const form = e.target;
+    const name = form.name.value;
+    const category = form.category.value;
+    const description = form.description.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseFloat(form.quantity.value);
+    const image = form.image.files[0];
+    //custom hook for image
+    const imageURL = await imageUpload(image);
+
+    //seller information
+    const seller = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      imageURL,
+      seller,
+    };
+    console.log(plantData)
+
+    // sent plantData in database
+    try {
+      // post req
+      const plant = await axiosSecure.post('/plants', plantData)
+      console.log(plant,'allllllllllllllllllllllllllllll plant')
+
+      toast.success("Data Added Successfully!");
+      form.reset()
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+
+
+
+
+   };
+
+  return (
+    <div>
+      <Helmet>
+        <title>Add Plant | Dashboard</title>
+      </Helmet>
+
+      {/* Form */}
+      <AddPlantForm
+        uploadImage={uploadImage}
+        setUploadImage={setUploadImage}
+        handleSubmit={handleSubmit}
+        loading={loading} />
+    </div>
+  );
+};
+
+export default AddPlant;
